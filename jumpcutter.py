e2a4ad398ad76f29679c88be374b6e24348cb009
riccardiunix <riccardo.bordon.2014@gmail.com>
2020-07-08 08:32:40 +0200

    not write in the file evrey time a new frame is choosen

--- jumpcutter.py
+++ jumpcutter.py
@@ -11,11 +11,11 @@ from scipy.io import wavfile
 def getMaxVolume(s):
     return max(float(np.max(s)), -float(np.min(s)))
 
-def copyFrame(inputFrame, outputFrame, fileFrame):
-    src = "TMP/{}.jpg".format((inputFrame + 1))
+def copyFrame(inputFrame, outputFrame, list):
+    src = "TMP/{}.jpg".format(inputFrame + 1)
     if not os.path.isfile(src):
         return False
-    fileFrame.write("file '{}'\n".format(src))
+    list.append("file '{}'\n".format(src))
     if outputFrame%500 == 499:
         print(str(outputFrame + 1) + " time-altered frames saved.")
     return True
@@ -46,7 +46,7 @@ parser.add_argument('--st', type=float, default=0.04, help="the volume amount th
 parser.add_argument('--sound', type=float, default=1.15, help="the speed that sounded (spoken) frames should be played at.")
 parser.add_argument('--silent', type=float, default=6.00, help="the speed that silent frames should be played at")
 parser.add_argument('--fm', type=float, default=1, help="some silent frames adjacent to sounded frames are included to provide context")
-parser.add_argument('--sr', type=float, default=32000, help="sample rate of the input and output videos")
+parser.add_argument('--sr', type=float, default=41000, help="sample rate of the input and output videos")
 parser.add_argument('-r', type=float, default=30, help="frame rate of the input and output videos.")
 parser.add_argument('-q', type=int, default=4, help="quality of frames to be extracted from input video.")
 
@@ -70,7 +70,7 @@ AUDIO_FADE_ENVELOPE_SIZE = 400
     
 
 
-command = "ffmpeg -i '{}' -qscale:v {} TMP/%d.jpg -hide_banner".format(INPUT_FILE, FRAME_QUALITY,)
+command = "ffmpeg -i '{}' -qscale:v {} TMP/%d.jpg -hide_banner".format(INPUT_FILE, FRAME_QUALITY)
 subprocess.call(command, shell=True)
 
 command = "ffmpeg -i '{}' -ab 160k -ac 2 -ar '{}' -vn TMP/audio.wav".format(INPUT_FILE, SAMPLE_RATE)
@@ -110,11 +110,11 @@ outputPointer = 0
 
 lastExistingFrame = None
 fileFrame = open("fileFrame.txt", "w")
+sFile = 'TMP/tempStart.wav'
+eFile = 'TMP/tempEnd.wav'
 for chunk in chunks:
     audioChunk = audioData[int(chunk[0]*samplesPerFrame) : int(chunk[1]*samplesPerFrame)]
     
-    sFile = 'TMP/tempStart.wav'
-    eFile = 'TMP/tempEnd.wav'
     wavfile.write(sFile, SAMPLE_RATE, audioChunk)
     with WavReader(sFile) as reader:
         with WavWriter(eFile, reader.channels, reader.samplerate) as writer:
@@ -126,7 +126,7 @@ for chunk in chunks:
     outputAudioData = np.concatenate((outputAudioData, alteredAudioData/maxAudioVolume))
 
     if leng < AUDIO_FADE_ENVELOPE_SIZE:
-        outputAudioData[outputPointer : endPointer] = 0
+        outputAudioData[outputPointer:endPointer] = 0
     else:
         premask = np.arange(AUDIO_FADE_ENVELOPE_SIZE)/AUDIO_FADE_ENVELOPE_SIZE
         mask = np.repeat(premask[:, np.newaxis], 2, axis=1)
@@ -136,14 +136,16 @@ for chunk in chunks:
     
     startOutputFrame = int(math.ceil(outputPointer/samplesPerFrame))
     endOutputFrame = int(math.ceil(endPointer/samplesPerFrame))
+    frameList = []
     for outputFrame in range(startOutputFrame, endOutputFrame):
         inputFrame = int(chunk[0] + NEW_SPEED[int(chunk[2])] * (outputFrame-startOutputFrame))
-        didItWork = copyFrame(inputFrame, outputFrame, fileFrame)
+        didItWork = copyFrame(inputFrame, outputFrame, frameList)
         if didItWork:
             lastExistingFrame = inputFrame
         elif lastExistingFrame != None:
-            copyFrame(lastExistingFrame, outputFrame, fileFrame)
-
+            copyFrame(lastExistingFrame, outputFrame, frameList)
+    
+    fileFrame.write(''.join(frameList))
     outputPointer = endPointer
 fileFrame.close()
 
